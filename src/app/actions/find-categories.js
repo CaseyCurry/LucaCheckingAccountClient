1c5a40c7c30a79d72b4667e397a24207af2dc20f
CaseyCurry <casey.de.curry@gmail.com>
2017-09-10 15:46:42 -0500

    update call to categories to use api-proxy

--- src/app/actions/find-categories.js
+++ src/app/actions/find-categories.js
@@ -1,12 +1,14 @@
 export default (api) => {
-  return {
-    type: "FIND_CATEGORIES",
-    payload: api.getUrl()
-      .then(url => {
-        return api.client.get(url);
-      })
-      .catch(error => {
-        Promise.reject(error);
-      })
+  return () => {
+    return {
+      type: "FIND_CATEGORIES",
+      payload: api.getUrl()
+        .then(url => {
+          return api.client.get(url);
+        })
+        .catch(error => {
+          Promise.reject(error);
+        })
+    };
   };
 };
